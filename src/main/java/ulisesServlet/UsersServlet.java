755ecd33c2a581d6a2c9b18da8f91f606013b75e
Oleksandr Dovbysh <alexdovbysh@gmail.com>
2014-04-29 18:24:37 +0200

    trying establish connection with android

--- src/main/java/ulisesServlet/UsersServlet.java
+++ src/main/java/ulisesServlet/UsersServlet.java
@@ -4,6 +4,7 @@ package ulisesServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,9 +19,7 @@ import wiamDB.Users;
 /*
  * UsersServlet
  * 
- * @Author: Oleksander Dovbysh
- * 			Elisabet Navarro
- * 			Sheila Perez
+ * @Author: Oleksander Dovbysh Elisabet Navarro Sheila Perez
  * 
  * This is free software, licensed under the GNU General Public License v3. See
  * http://www.gnu.org/licenses/gpl.html for more information.
@@ -68,17 +67,18 @@ public class UsersServlet extends HttpServlet {
 		 * request.getPart("password").toString(); System.out.println("" +
 		 * userName + ":" + pass);
 		 */
+		System.out.println("Get: " + user + ":" + pass);
 		// check if user exists in db
 		boolean exist = false;
 		try {
-			exist = u.selectUserByName(user,pass);
+			exist = u.selectUserByName(user, pass);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		if (exist) {
 			// accio depenent de comment o valoration
 		} else {
-			u.insertUser(user,pass);
+			u.insertUser(user, pass);
 		}
 		// output data
 		PrintWriter out = response.getWriter();
@@ -90,46 +90,47 @@ public class UsersServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// database conection
-				Users u = new Users();
-				try {
-					u.connect();
-				} catch (ClassNotFoundException e1) {
-					e1.printStackTrace();
-				}
-				// type of response dates
-				response.setContentType("application/json");
-				response.setCharacterEncoding("utf-8");
-				// input client dates
-				JSONObject jObj = null;
-				String user = "";
-				String pass = "";
-				try {
-					jObj = new JSONObject(request.getParameter("mydata"));
-					user = jObj.getString("user");
-					pass = jObj.getString("password");
-				} catch (JSONException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				System.out.println("" + user + ":" + pass + ":" + jObj.toString());
-				// check if user exists in db
-				boolean exist = false;
-				try {
-					exist = u.selectUserByName(user,pass);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				if (exist) {
-					// accio depenent de comment o valoration
-				} else {
-					u.insertUser(user,pass);
-				}
-				// output data
-				PrintWriter out = response.getWriter();
-				out.print(exist);
-				out.flush();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+		// database conection
+		Users u = new Users();
+		try {
+			u.connect();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		// type of response dates
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+		// input client dates
+		JSONObject jObj = null;
+		String user = "";
+		String pass = "";
+		try {
+			jObj = new JSONObject(request.getParameter("mydata"));
+			user = jObj.getString("user");
+			pass = jObj.getString("password");
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("Post: " + user + ":" + pass + ":" + jObj.toString());
+		// check if user exists in db
+		boolean exist = false;
+		try {
+			exist = u.selectUserByName(user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if (exist) {
+			// accio depenent de comment o valoration
+		} else {
+			u.insertUser(user, pass);
+		}
+		// output data
+		PrintWriter out = response.getWriter();
+		out.print(exist);
+		out.flush();
 	}
 	
 }
